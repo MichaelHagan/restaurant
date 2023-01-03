8caa1927d77286e2e783fc3add47141dc1f371aa
Tracy Sarah Afram-Owusu <aframowusutracysarah@gmail.com>
2023-01-03 10:19:08 +0000

    change footer text color

--- frontend2/src/components/footer/Footer.js
+++ frontend2/src/components/footer/Footer.js
@@ -6,23 +6,23 @@ const Footer = () => {
     <div className='w-full h-auto'>
       <footer className="footer p-10 bg-base-200 text-base-content">
         <div>
-          <img src={logo} alt="logo" width="80" height="160" />
+          <img src={logo} alt="logo" width="60" />
           <p>Order Me.<br />Providing good food</p>
         </div>
         <div>
-          <span className="footer-title">Quick Links</span>
+          <span className="text-yellow-300 text-sm">Quick Links</span>
           <a href='/#home' className="link link-hover">Home</a>
           <a href='/#category' className="link link-hover">Categories</a>
           <a href="/#about" className="link link-hover">About</a>
           <a href="/#contact" className="link link-hover">Contact</a>
         </div>
         <div>
-          <span className="footer-title">Social Media</span>
+          <span className="text-yellow-300 text-sm">Social Media</span>
           <a className="link link-hover">Facebook</a>
           <a className="link link-hover">Instagram</a>
         </div>
         <div>
-          <span className="footer-title">Legal</span>
+          <span className="text-yellow-300 text-sm">Legal</span>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
