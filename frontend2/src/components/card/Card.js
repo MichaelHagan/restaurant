587acabfd7a97365ec2770feeb4e4bd5a7c7181f
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2023-01-12 14:05:16 +0000

    Price_AddToCart

--- frontend2/src/components/card/Card.js
+++ frontend2/src/components/card/Card.js
@@ -4,15 +4,15 @@ const Card = ({ id, name, desc, price, available }) => {
 
 
   return (
-    <div className={available ? "shadow-md rounded-xl shadow-[#957bcb]" : 'overlay'}>
+    <div className={available ? "shadow-md rounded-xl shadow-[#957bcb] h-[500px] lg:h-[450px]" : 'overlay'}>
       <figure><img src="food.jpg" /></figure>
-      <div className="card-body">
-        <h2 className="card-title text-gray-600">{name}</h2>
-        <p className='text-left'>{desc}</p>
-        <p className='text-left text-sm text-red-500'>Ghc{price}</p>
+      <div className="card-body relative border-2 h-[250px]  md:h-[250px] lg:h-[230px] w-full">
+        <h2 className="card-title text-gray-600 text-left">{name}</h2>
+        <p className='text-left text-sm'>{desc}</p>
         {/* <span className='text-right'>Unavailable</span> */}
-        <div className="card-actions justify-end">
-          <button className="btn btn-warning"> Add to cart</button>
+        <div className="flex flex-row border-2 mt-24 gap-12  items-center absolute right-0 bottom-0 ">
+          <p className='text-md text-red-500 font-bold'>Ghc{price}</p>
+            <button className="btn btn-warning mr-2 "> Add to cart</button>
         </div>
       </div>
     </div>
--- frontend2/src/components/categorylist/CategoryList.js
+++ frontend2/src/components/categorylist/CategoryList.js
@@ -5,14 +5,14 @@ import Card from '../card/Card';
 const CategoryList = ({ Category, List, selectHandler }) => {
 
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-full">
       <div className="max-w-[1240px] m-auto flex flex-col justify-center w-full px-2 py-24">
         <div>
         <h2 className="text-3xl text-gray-600 font-bold inline border-b-4 border-yellow-300">
           {Category}
         </h2>
         </div>
-        <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8">
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center py-8">
           {List.map((food) => (
             <div key={food.id}
               onClick={event => selectHandler(
