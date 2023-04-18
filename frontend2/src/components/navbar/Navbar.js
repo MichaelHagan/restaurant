d4eee2276c30bdf1b1ae339af43bf0d43a9d1ec4
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2023-04-18 10:06:56 +0000

    cleared_images

--- frontend2/src/components/navbar/Navbar.js
+++ frontend2/src/components/navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import logo from "../../images/logo.png";
-import { FaShoppingCart } from "react-icons/fa";
 import Trigger from "../sidetrigger/Trigger";
 import Cart from '../cart/Cart';
 
